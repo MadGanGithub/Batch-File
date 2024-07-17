a27580fcecc7e6ce8d4526cc8cf5c5e2eac36292
MadGanGithub <madhavganesan95@gmail.com>
2024-07-17 20:36:07 +0530

    intial_commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## Batch file for MERN stack setup
---
+++ setup.bat
@@ -0,0 +1,57 @@
+@echo off
+title MERN Stack setup!
+echo 1)Git initialization
+git init
+echo Git initialization is completed
+echo Press any key to continue....
+pause
+echo 2)Backend part
+mkdir backend
+cd backend
+start /wait cmd /c "npm i dotenv express jsonwebtoken body-parser bcrypt cookie-parser mongoose uuid cors cross-env"npm i nodemon --save-dev
+echo Backend dependencies installed successfully
+echo Press any key to continue....
+pause
+echo 3)Folder structure
+echo Index file creation
+type nul > index.js
+echo const app = require("./app.js"); >> index.js
+echo. >> index.js
+echo app.listen(4000, ()=^>{ >> index.js
+echo     console.log("Started at port: 4000"); >> index.js
+echo }); >> index.js
+echo Press any key to continue....
+pause
+echo App file creation
+type nul > app.js
+echo import cors from "cors"; >> app.js
+echo import express from "express"; >> app.js
+echo. >> app.js
+echo const app=express(); >> app.js
+echo. >> app.js
+echo app.use(cors({ >> app.js
+echo    origin: 'http://localhost:3000', >> app.js
+echo    credentials: true, >> app.js
+echo    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], >> app.js
+echo    allowedHeaders: ['Content-Type', 'Authorization'], >> app.js
+echo })); >> app.js
+echo. >> app.js
+echo export default app; >> app.js
+echo Press any key to continue....
+pause
+echo Folder creation
+mkdir models
+mkdir routes
+mkdir controllers
+mkdir config
+cd config 
+type nul > config.env
+cd ..
+echo Folder creation completed successfully
+cd ..
+echo 4)Frontend part
+start /wait cmd /c "npx create-react-app frontend"
+cd frontend
+start /wait cmd /c "npm i react-router-dom axios"
+echo Frontend dependencies installed successfully
+echo "Installation completed!!"
